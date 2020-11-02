f4f3bc7056cb31fcc98458b65148ef1d328a05b2
firststef <firstbossspike@gmail.com>
2020-11-02 20:04:50 +0200

    Change fault in code

--- tema1/main.py
+++ tema1/main.py
@@ -37,7 +37,7 @@ def split_into_chunks(text, chunk_size):
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
 
-def xor_bytestrings(TE)
+def xor_bytestrings():pass
 
 
 def ecb_encrypt(text, key: bytes, chunk_size):
